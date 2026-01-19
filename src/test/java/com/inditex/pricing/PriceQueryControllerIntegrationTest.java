a336cdc5a3da524a783c5ff2e6d8b42fff8b65c4
mondrca <cmondragonr@hotmail.com>
2026-01-19 20:08:07 +0100

    chore: simplify configuration and align profiles

--- src/test/java/com/inditex/pricing/PriceQueryControllerIntegrationTest.java
+++ src/test/java/com/inditex/pricing/PriceQueryControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,10 +13,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class PriceQueryControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+    
 
     // Test 1: 2020-06-14 10:00 -> price_list 1, price 35.50
     @Test
